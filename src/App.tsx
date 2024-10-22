6b82eae1e21a64e9a6ede373f7f92dcbe03a00b6
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-10-22 16:15:04 +0200

    Ajout props sur PokemonCard et utilisation props sur App git push git push clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
     <div>
       <h1>Liste des Pokémons</h1>
       <figure>
         <figcaption>
-          <PokemonCard />
+          <PokemonCard pokemon={pokemonList[1]} />
         </figcaption>
       </figure>
     </div>
