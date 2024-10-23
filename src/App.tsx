e4f452cf6e79b433d1676f89edc58ac6863c3167
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-10-23 15:51:06 +0200

    Ajout state et changement de tableau

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -10,17 +11,54 @@ const pokemonList = [
   },
 
   {
+    name: "charmander",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+
+  {
+    name: "squirtle",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+
+  {
+    name: "pikachu",
+
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+
+  {
     name: "mew",
   },
 ];
-
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setPokemonIndex((Index) =>
+      Index < pokemonList.length - 1 ? Index + 1 : Index,
+    );
+  };
+
+  const handlePreviousClick = () => {
+    setPokemonIndex((Index) => (Index > 0 ? Index - 1 : Index));
+  };
   return (
     <div>
       <h1>Liste des Pokémons</h1>
       <figure>
         <figcaption>
-          <PokemonCard pokemon={pokemonList[0]} />
+          <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+          <button type="button" onClick={handlePreviousClick}>
+            Précedent
+          </button>
+          <button type="button" onClick={handleNextClick}>
+            Suivant
+          </button>
         </figcaption>
       </figure>
     </div>
